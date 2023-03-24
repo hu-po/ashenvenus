d3c59b56536334751be108378d6fd7d58c009158
HugoCMU <hugo.p.cmu@gmail.com>
2023-03-24 18:38:09 -0500

    seperate valid and eval

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def train_eval_loop(
+def train_valid_eval_loop(
     train_dir: str = "data/train/1",
     eval_dir: str = "data/test/a",
     output_dir: str = "output",
@@ -31,12 +31,19 @@ def train_eval_loop(
     # model = model.to(device)
 
     model = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet',
-        in_channels=32, out_channels=1, init_features=32, pretrained=False)
+                           in_channels=32, out_channels=1, init_features=32, pretrained=False)
     model.to(device)
 
     # Training dataset
     train_dataset = FragmentPatchesDataset(train_dir, viz=False, train=True)
-    train_sampler = RandomSampler(train_dataset, replacement=True, num_samples=train_dataset_size)
+
+    # Sampler for Train and Validation
+    train_sampler = RandomSampler(
+        train_dataset, replacement=True, num_samples=train_dataset_size)
+    valid_sampler = RandomSampler(
+        train_dataset, replacement=False, num_samples=eval_dataset_size)
+
+    # DataLoaders
     train_dataloader = DataLoader(
         train_dataset,
         batch_size=batch_size,
@@ -47,16 +54,12 @@ def train_eval_loop(
         # This will make it go faster if it is loaded into a GPU
         pin_memory=True,
     )
-
-    # Evaluation dataset
-    eval_dataset = FragmentPatchesDataset(eval_dir, viz=False, train=False)
-    eval_sampler = RandomSampler(eval_dataset, replacement=False, num_samples=eval_dataset_size)
-    eval_dataloader = DataLoader(
-        eval_dataset,
+    valid_dataloader = DataLoader(
+        train_dataset,
         batch_size=batch_size,
         # Shuffle does NOT work
         shuffle=False,
-        sampler=eval_sampler,
+        sampler=valid_sampler,
         num_workers=16,
         # This will make it go faster if it is loaded into a GPU
         pin_memory=True,
@@ -76,7 +79,7 @@ def train_eval_loop(
         # Train
         for patch, mask, label in tqdm(train_dataloader):
             patch = patch.to(device)
-            label = label.to(device).unsqueeze(1).astype(torch.float32)
+            label = label.to(device).unsqueeze(1).to(torch.float32)
             pred = model(patch)
             loss = loss_fn(pred, label)
             loss.backward()
@@ -84,14 +87,51 @@ def train_eval_loop(
             optimizer.zero_grad()
 
         # Test
-        for patch, mask in tqdm(eval_dataloader):
+        eval_score = 0
+        for patch, mask, label in tqdm(valid_dataloader):
             patch = patch.to(device)
+            label = label.to(device).unsqueeze(1).to(torch.float32)
             with torch.no_grad():
                 pred = model(patch)
                 loss = loss_fn(pred, label)
+                eval_score += loss.item()
 
+        if eval_score > best_eval_score:
+            best_eval_score = eval_score
+            torch.save(model.state_dict(), f"{output_dir}/model.pth")
 
     return best_eval_score
+
+
+def evaluate(
+    model,
+    loss_fn,
+    device,
+    data_dir="data/test/a",
+    batch_size: int = 32,
+):
+
+    eval_dataset = FragmentPatchesDataset(data_dir, viz=False, train=False)
+    # Sampler that goes through the entire dataset once with appropraite stride
+    eval_sampler = RandomSampler(eval_dataset, replacement=False)
+    eval_dataloader = DataLoader(
+        eval_dataset,
+        batch_size=batch_size,
+        # Shuffle does NOT work
+        shuffle=False,
+        sampler=eval_sampler,
+        num_workers=16,
+        # This will make it go faster if it is loaded into a GPU
+        pin_memory=True,
+    )
+
+    for patch, mask in tqdm(eval_dataloader):
+        patch = patch.to(device)
+        with torch.no_grad():
+            pred = model(patch)
+
+    # TODO: RLE dump to file
+            
 
 
 if __name__ == '__main__':

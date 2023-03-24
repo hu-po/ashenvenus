1106644788893cccb8b430c588e596973170ea45
HugoCMU <hugo.p.cmu@gmail.com>
2023-03-24 13:48:52 -0500

    debugging dataset

--- dataset.py
+++ dataset.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import PIL.Image as Image
 import torch.utils.data as data
-from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import ToTensor
 from tqdm import tqdm
 import random
@@ -31,11 +30,9 @@ def load_image(image_filepath: str = 'image.png', viz: bool = False) -> torch.Te
     log.debug(f"\tImage std: {_pt.std()}")
     if viz:
         plt.title(image_filepath)
-        _pt_np = _pt.numpy()
-        if _pt_np.shape[0] in [1, 3, 4]:  # If there are 1, 3, or 4 channels
-            # Move the channel axis to the last position
-            _pt_np = np.transpose(_pt_np, (1, 2, 0))
-        plt.imshow(_pt_np, cmap='gray')
+        # Move the channel axis to the last position
+        _pt = np.transpose(_pt.numpy(), (1, 2, 0))
+        plt.imshow(_pt, cmap='gray')
         plt.show()
     return _pt
 
@@ -54,9 +51,9 @@ class FragmentPatchesDataset(data.Dataset):
         # Expected slices per fragment
         expected_slice_count: int = 65,
         # Size of an individual patch
-        patch_size_x: int = 64,
-        patch_size_y: int = 64,
-        patch_size_z: int = 4,
+        patch_size_x: int = 1028,
+        patch_size_y: int = 1028,
+        patch_size_z: int = 32,
     ):
         log.info(f"Initializing FragmentDataset with data_dir={data_dir}")
         # Verify paths and directories for images and metadata
@@ -101,7 +98,8 @@ class FragmentPatchesDataset(data.Dataset):
         # Load the slices (tif files) into one tensor, pre-allocate
         self.fragment = torch.zeros(
             self.expected_slice_count, self.fragment_size_x, self.fragment_size_y)
-        for i, slice_path in enumerate(tqdm(glob.glob(os.path.join(self.slices_dir, '*.tif')))):
+        for i in tqdm(range(self.expected_slice_count)):
+            slice_path = os.path.join(self.slices_dir, f"{i:02d}.tif")
             _slice = load_image(slice_path)
             self.fragment[i, :, :] = _slice
 
@@ -122,21 +120,38 @@ class FragmentPatchesDataset(data.Dataset):
             2], f"Patch size y ({self.patch_size_y}) is larger than the fragment height ({self.fragment.shape[2]})"
         assert self.patch_size_z <= self.fragment.shape[
             0], f"Patch size z ({self.patch_size_z}) is larger than the fragment depth ({self.fragment.shape[0]})"
-        
+
         # Store the half-sizes of the patches
         self.patch_half_size_x = self.patch_size_x // 2
         self.patch_half_size_y = self.patch_size_y // 2
         self.patch_half_size_z = self.patch_size_z // 2
 
         # Pad the fragment to make sure we can get patches from the edges
-        self.fragment = torch.nn.functional.pad(self.fragment, (self.patch_half_size_x, self.patch_half_size_x, self.patch_half_size_y, self.patch_half_size_y, self.patch_half_size_z, self.patch_half_size_z))
+        pad_sizes = (self.patch_half_size_x, self.patch_half_size_x, self.patch_half_size_y,
+                     self.patch_half_size_y, self.patch_half_size_z, self.patch_half_size_z)
+        self.fragment = torch.nn.functional.pad(
+            self.fragment, pad_sizes, value=0.0)
 
-        # TODO: Make sure only indices that are within the mask are valid
+        # Pad the mask, labels, and IR images
+        pad_sizes = (self.patch_half_size_x, self.patch_half_size_x, self.patch_half_size_y,
+                     self.patch_half_size_y, 0, 0) # No padding on z
+        self.mask_padded = torch.nn.functional.pad(
+            self.mask, pad_sizes, value=0.0)
+        log.debug(f"Mask padded: {self.mask_padded.shape}")
+        self.labels_padded = torch.nn.functional.pad(
+            self.labels, pad_sizes, value=0.0)
+        log.debug(f"Labels padded: {self.labels_padded.shape}")
+        
+        # Make sure all the padded images are the same size
+        assert self.mask_padded.shape == self.labels_padded.shape, f"Mask and labels are not the same size: {self.mask_padded.shape} vs {self.labels_padded.shape}"
+        assert self.mask_padded.shape[1:] == self.fragment.shape[1:], f"Mask and fragment are not the same size: {self.mask_padded.shape} vs {self.fragment.shape}"
+
+        # TODO: Filter indices to only those within the mask?
 
     def __len__(self):
         return self.fragment_size_x * self.fragment_size_y * self.fragment_size_z
 
-    def __getitem__(self, index, viz=False):
+    def __getitem__(self, index, viz_patch=False, viz_patch_full=False):
         # Get the x, y, and z indices
         z = index // (self.fragment_size_x * self.fragment_size_y)
         y = (index - z * self.fragment_size_x *
@@ -150,29 +165,72 @@ class FragmentPatchesDataset(data.Dataset):
 
         # Get the patch and label
         patch = self.fragment[
-            z - self.patch_half_size_x : z + self.patch_half_size_x,
-            x - self.patch_half_size_y : x + self.patch_half_size_y,
-            y - self.patch_half_size_z : y + self.patch_half_size_z,
+            z - self.patch_half_size_z: z + self.patch_half_size_z,
+            x - self.patch_half_size_x: x + self.patch_half_size_x,
+            y - self.patch_half_size_y: y + self.patch_half_size_y,
         ]
+        log.debug(f"Patch shape is {patch.shape}")
+        log.debug(f"\t\t Patch type is {patch.dtype}")
+        log.debug(f"\t\t Patch min is {patch.min()}")
+        log.debug(f"\t\t Patch max is {patch.max()}")
+        log.debug(f"\t\t Patch mean is {patch.mean()}")
         # TODO: Should label be average of patch? center of patch?
         label = self.labels[0, x, y]
+        log.debug(f"Label is {label}")
 
-        if viz:
-            # Visualize the patch
-            fig, ax = plt.subplots(1, self.patch_size_z + 1)
+        if viz_patch:
+            # Visualize the patch bbox, mask patch, label patch, and patch itself
+            fig, ax = plt.subplots(1, 3, figsize=(20, 20))
+
             # Visualize the patch in the infrared image
-            _pt_np = self.ir.numpy()
-            if _pt_np.shape[0] in [1, 3, 4]:  # If there are 1, 3, or 4 channels
-                # Move the channel axis to the last position
-                _pt_np = np.transpose(_pt_np, (1, 2, 0))
+            ax[0].set_title("Patch in IR image")
+            _pt_np = np.transpose(self.ir.numpy(), (1, 2, 0))
             ax[0].imshow(_pt_np, cmap='gray')
-            bbox_anchor = (y - self.patch_half_size_y, x - self.patch_half_size_x)
+            bbox_anchor = (y - self.patch_half_size_y,
+                           x - self.patch_half_size_x)
             bbox = patches.Rectangle(
                 bbox_anchor, self.patch_size_y, self.patch_size_x, linewidth=10, edgecolor='r', facecolor='none')
             ax[0].add_patch(bbox)
+
+            # Visualize the mask of the patch
+            ax[1].set_title("Mask patch")
+            _pt_np =self.mask_padded[
+                0,
+                x - self.patch_half_size_x: x + self.patch_half_size_x,
+                y - self.patch_half_size_y: y + self.patch_half_size_y,
+            ]
+            # _pt_np = np.transpose(_pt_np, (1, 2, 0))
+            ax[1].imshow(_pt_np, cmap='gray')
+
+            # Visualize the label of the patch
+            ax[2].set_title(f"Label patch {label}")
+            _pt_np =self.labels_padded[
+                0,
+                x - self.patch_half_size_x: x + self.patch_half_size_x,
+                y - self.patch_half_size_y: y + self.patch_half_size_y,
+            ]
+            # _pt_np = np.transpose(_pt_np, (1, 2, 0))
+            ax[2].imshow(_pt_np, cmap='gray')
+            plt.show()
+
+        if viz_patch_full:
+            # Calculate the number of rows and columns
+            num_cols = int(np.ceil(np.sqrt(self.patch_size_z)))
+            num_rows = int(np.ceil(self.patch_size_z / num_cols))
+
             # Visualize each depth of the patch
+            fig, axs = plt.subplots(num_rows, num_cols, figsize=(20, 20))
+
+            # Flatten the axs array in case it's 2D
+            axs_flat = axs.flatten()
+
             for i in range(self.patch_size_z):
-                ax[i + 1].imshow(patch[:, :, i].numpy(), cmap='gray')
+                axs_flat[i].imshow(patch[i, :, :].numpy(), cmap='gray')
+
+            # Hide the remaining unused subplots if any
+            for i in range(self.patch_size_z, num_rows * num_cols):
+                axs_flat[i].axis('off')
+
             plt.show()
 
         return patch, label
@@ -193,4 +251,4 @@ if __name__ == '__main__':
     dataset = FragmentPatchesDataset(data_dir)
     for _ in range(3):
         i = random.randint(0, len(dataset))
-        patch, label = dataset.__getitem__(i, viz=True)
+        patch, label = dataset.__getitem__(i, viz_patch=True, viz_patch_full=True)
